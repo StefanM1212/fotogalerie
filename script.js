3d994f16c20bb8d8ad8d67dfa62981d660473f0c
Lions Performance <zugang@lionsperformance.de>
2026-04-01 07:04:27 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -11,11 +11,21 @@ const bilder = Array.from(galeriebilder).map(img => img.src);
 
 const fotoButtons = document.querySelectorAll('.foto-button');
 
+const photocard = document.querySelectorAll('.foto-karte');
+
+const aktuelleKarte = document.querySelector('.foto-karte.aktiv');
+
+const leftArrow = document.querySelector('.nav-pfeil-links');
+const rightArrow = document.querySelector('.nav-pfeil-rechts');
+
+
+
 fotoButtons.forEach(function(button, index) {
     button.addEventListener('click', function() {
         openOverlay(index);
     });
 });
+
 
 
 let aktuellerIndex = 0;
@@ -51,3 +61,82 @@ function naechstesBild() {
      event.stopPropagation();
     overlayBild.src = bilder[aktuellerIndex];
 }
+
+
+document.addEventListener('keydown', function(event) {
+    if (overlay.classList.contains('aktiv')) {
+        // Overlay ist offen → Bild wechseln
+        if (event.key === 'ArrowLeft') {vorherigesBild(); leftArrow.classList.add('pressed'); 
+ 
+            setTimeout(function() {
+        leftArrow.classList.remove('pressed');
+    }, 300);
+         }
+        
+        
+        
+         if (event.key === 'ArrowRight') {naechstesBild(); rightArrow.classList.add('pressed'); 
+
+setTimeout(function() {
+        rightArrow.classList.remove('pressed');
+    }, 300);
+
+
+
+
+
+         }
+
+    
+    }
+    else {
+        // Overlay ist geschlossen → Rahmen bewegen
+        if (event.key === 'ArrowLeft') aktivesKarteMinus();
+        if (event.key === 'ArrowRight') aktivesKartePlus();
+    }
+
+
+});
+
+
+
+
+
+function aktivesKartePlus() {
+    const aktuelleKarte = document.querySelector('.foto-karte.aktiv');
+    if (aktuelleKarte) aktuelleKarte.classList.remove('aktiv');
+    
+    aktuellerIndex = (aktuellerIndex + 1) % photocard.length;
+    photocard[aktuellerIndex].classList.add('aktiv');
+}
+
+function aktivesKarteMinus() {
+    const aktuelleKarte = document.querySelector('.foto-karte.aktiv');
+    if (aktuelleKarte) aktuelleKarte.classList.remove('aktiv');
+    
+    aktuellerIndex = (aktuellerIndex - 1 + photocard.length) % photocard.length;
+    photocard[aktuellerIndex].classList.add('aktiv');
+}
+
+
+document.addEventListener('keydown', enertoopen);
+
+function enertoopen (event) { 
+const aktuelleKarte = document.querySelector('.foto-karte.aktiv');
+if (event.key === 'Enter') {
+    openOverlay(Array.from(photocard).indexOf(aktuelleKarte));
+}
+}
+
+document.addEventListener('keydown', pressexit);
+
+function pressexit (event) {
+if (event.key === 'Escape') {
+    schliesseOverlay();
+}
+}
+
+
+
+
+
